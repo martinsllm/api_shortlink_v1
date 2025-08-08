2bdde51aabdd42eb0b4ba35863d2da81d5bd194b
martinsllm <lucaslm2010@gmail.com>
2025-08-08 20:59:07 -0300

    Login and token creation

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\LoginController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/login', [LoginController::class, 'login'])
+    ->middleware('guest')
+    ->name('login');
